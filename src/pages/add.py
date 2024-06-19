a48083ff022a64526029b695fb167ae3868d33d4
Mugen-N <vanagutorov@gmail.com>
2024-06-19 23:01:41 +0500

    Update add.py

--- src/pages/add.py
+++ src/pages/add.py
@@ -23,7 +23,7 @@ token = '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'
 # Ваш репозиторий в формате "username/repo"
 repo = 'Mugen-N/Augmentation_online'
 # Папка в репозитории, куда вы хотите загрузить изображение
-upload_folder = 'Upload_dataset/user'
+upload_folder = 'Upload_dataset/User3'
 
 def main():
     menu()
